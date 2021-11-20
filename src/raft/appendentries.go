71b601ee28029e49d20414ed53086c54e37f04da
liyechen <gliyechen@hotmail.com>
2021-11-20 18:18:30 +0800

    chore: define AppendEntries

--- src/raft/appendentries.go
+++ src/raft/appendentries.go
@@ -2,22 +2,33 @@ package raft
 
 // heartbeat
 type AppendEntriesArgs struct {
-	PeerID int
+	Term         int // leader's term
+	LeaderID     int // leader's ID
+	PrevLogIndex int // index of log entry immediately preceding new ones
+	PrevLogTerm  int // term of PrevLogIndex entry
+	LeaderCommit int // leader's commit index
+
+	// TODO: use entry struct, log entries to store
+	// (empty for heartbeat; may send more than one for efficiency)
+	Entries []int
 }
 
 type AppendEntriesReply struct {
-	Ack bool
+	Term    int  // current term for leader to update itself
+	Success bool // true if follower contained entry matching prevLogIndex and prevLogTerm
 }
 
+// NYI:
 func (rf *Raft) AppendEntriesHandler(args *AppendEntriesArgs, reply *AppendEntriesReply) {
-	rf.receiveLeaderAppendEntriesCh <- true
+	rf.receiveAppendEntriesCh <- true
 }
 
+// NYI:
 func (rf *Raft) appendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
 	rf.rwmu.RLock()
 	defer rf.rwmu.RUnlock()
 
-	args.PeerID = rf.me
+	args.LeaderID = rf.me
 	ok := rf.peers[server].Call("Raft.AppendEntriesHandler", args, reply)
 	return ok
 }
